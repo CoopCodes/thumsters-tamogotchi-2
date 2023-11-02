66efb59b5e6bc2681fea68ed2164a0ee8da90b85
CoopCodes <gc021217@goldcoastcc.qld.edu.au>
2023-11-02 17:19:51 +1000

    Continueing to try and implement drag and drop

--- thumsters-tamogotchi-3/components/ListBodyPart.tsx
+++ thumsters-tamogotchi-3/components/ListBodyPart.tsx
@@ -2,6 +2,7 @@ import { useRef, useState, useContext } from "react";
 import { BodyPart, theme, OnRemoveType, nodeRangeThreshold } from "../global";
 import { Animated, PanResponder, View, Image, StyleSheet } from "react-native";
 import { MonsterContext } from "../Contexts/MonsterContext";
+import DraggableView from "react-native-draggable-reanimated";
 
 // import Animated, {
 //   useAnimatedStyle,
@@ -21,99 +22,114 @@ interface Props {
 }
 
 const ListBodyPart = ({ bodypart }: Props) => {
-  const pan = useRef(new Animated.ValueXY()).current;
+  // const pan = useRef(new Animated.ValueXY()).current;
   const [isDragging, setIsDragging] = useState(false);
   const [withinNode, setWithinNode] = useState(false);
   const { monster, monsterDispatch } = useContext(MonsterContext);
 
   // ***** Pan Responder implementation: ***** //
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderGrant: () => {
-        setIsDragging(true);
-        // Set the initial position of the
-        // component to the finger position
+  // const panResponder = useRef(
+  //   PanResponder.create({
+  //     onStartShouldSetPanResponder: () => true,
+  //     onPanResponderGrant: () => {
+  //       setIsDragging(true);
+  //       // Set the initial position of the
+  //       // component to the finger position
 
-        pan.setValue({ x: bodypart.node[0], y: bodypart.node[1] });
-        // The above line positions the
-        // bodypart at the nodes position, under the finger.
+  //       pan.setValue({ x: bodypart.node[0], y: bodypart.node[1] });
+  //       // The above line positions the
+  //       // bodypart at the nodes position, under the finger.
 
-        // Logic for detecting if it is close
-        // to another node, thus connecting it to the body:
-      },
-      onPanResponderMove: (e, gs) => {
-        // Move event and gesture object from panResponder
-        Animated.event([null, { dx: pan.x, dy: pan.y }], {
-          useNativeDriver: false,
-        })(e, gs); // Pass event and gesture object to Animated.event function
+  //       // Logic for detecting if it is close
+  //       // to another node, thus connecting it to the body:
+  //     },
+  //     onPanResponderMove: (e, gs) => {
+  //       // Move event and gesture object from panResponder
+  //       Animated.event([null, { dx: pan.x, dy: pan.y }], {
+  //         useNativeDriver: false,
+  //       })(e, gs); // Pass event and gesture object to Animated.event function
 
-        // Check if position is within any node
-        let currentValue: { x: number; y: number } = {} as any;
+  //       // Check if position is within any node
+  //       let currentValue: { x: number; y: number } = {} as any;
 
-        // pan.addListener((value: { x: number; y: number }) => {
-        // });
+  //       // pan.addListener((value: { x: number; y: number }) => {
+  //       // });
 
-        currentValue = {
-          x: Math.round(gs.moveX),
-          y: Math.round(gs.moveY)
-        };
+  //       currentValue = {
+  //         x: Math.round(gs.moveX),
+  //         y: Math.round(gs.moveY)
+  //       };
 
-        if (monster !== undefined)
-          Object.values(monster.bodypartnodesRelToVP).filter(value => value !== undefined).map(
-            (node: Array<number>) => {
-              if (
-                currentValue.x >= node[0] - nodeRangeThreshold &&
-                currentValue.x >= node[0] + nodeRangeThreshold &&
-                currentValue.y >= node[1] - nodeRangeThreshold &&
-                currentValue.y >= node[1] + nodeRangeThreshold
-              ) {
-                // Within `nodeRangeThreshold` of a node
-                setWithinNode(true);
-              } else {
-                setWithinNode(false);
-              }
-            }
-          );
-      },
-      onPanResponderRelease: () => {
-        // setIsDragging(false);
-        if (!withinNode) {
-          Animated.spring(
-            pan, // Auto-multiplexed
-            {toValue: {x: 0, y: 0}, useNativeDriver: true}, // Back to zero
-          ).start();
-        }
-        pan.flattenOffset();
-      },
-    })
-  ).current;
+  //       if (monster !== undefined)
+  //         Object.values(monster.bodypartnodesRelToVP).filter(value => value !== undefined).map(
+  //           (node: Array<number>) => {
+  //             if (
+  //               currentValue.x >= node[0] - nodeRangeThreshold &&
+  //               currentValue.x >= node[0] + nodeRangeThreshold &&
+  //               currentValue.y >= node[1] - nodeRangeThreshold &&
+  //               currentValue.y >= node[1] + nodeRangeThreshold
+  //             ) {
+  //               // Within `nodeRangeThreshold` of a node
+  //               setWithinNode(true);
+  //             } else {
+  //               setWithinNode(false);
+  //             }
+  //           }
+  //         );
+  //     },
+  //     onPanResponderRelease: () => {
+  //       // setIsDragging(false);
+  //       if (!withinNode) {
+  //         Animated.spring(
+  //           pan, // Auto-multiplexed
+  //           {toValue: {x: 0, y: 0}, useNativeDriver: true}, // Back to zero
+  //         ).start();
+  //       }
+  //       pan.flattenOffset();
+  //     },
+  //   })
+  // ).current;
 
-  return (
-    <View style={styles.parent}>
-      <View style={styles.main}>
-        <Animated.View
-          style={[
-            [
-              {
-                transform: [{ translateX: pan.x }, { translateY: pan.y }],
-              },
-              isDragging && { opacity: 1 },
-            ],
-          ]}
-          {...panResponder.panHandlers}
-        >
-          <Image style={[{
-            width: `${100 * bodypart.aspectRatio[0]}%`,
-            height: `${100 * bodypart.aspectRatio[1]}%`
-            }, styles.image]} source={bodypart.image} />
-        </Animated.View>
-      </View>
-      <View style={styles.shadow} />
-    </View>
-  );
+  // return (
+  //   <View style={styles.parent}>
+  //     <View style={styles.main}>
+  //       <Animated.View
+  //         style={[
+  //           [
+  //             {
+  //               transform: [{ translateX: pan.x }, { translateY: pan.y }],
+  //             },
+  //             isDragging && { opacity: 1 },
+  //           ],
+  //         ]}
+  //         {...panResponder.panHandlers}
+  //       >
+  //         <Image style={[{
+  //           width: `${100 * bodypart.aspectRatio[0]}%`,
+  //           height: `${100 * bodypart.aspectRatio[1]}%`
+  //           }, styles.image]} source={bodypart.image} />
+  //       </Animated.View>
+  //     </View>
+  //     <View style={styles.shadow} />
+  //   </View>
+  // );
 
   // ***** React native reanimated implementation: ***** //
+  return (
+    <DraggableView>
+      <Image
+        style={[
+          {
+            width: `${100 * bodypart.aspectRatio[0]}%`,
+            height: `${100 * bodypart.aspectRatio[1]}%`,
+          },
+          styles.image,
+        ]}
+        source={bodypart.image}
+      />
+    </DraggableView>
+  );
+
   // const onGestureEvent = useAnimatedGestureHandler<
   //   PanGestureHandlerGestureEvent,
   //   { x: number; y: number }
